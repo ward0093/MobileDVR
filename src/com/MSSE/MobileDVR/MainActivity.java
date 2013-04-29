d1bcc552e5a6be2555bbe76cf85b040dd572778b
Darin <darin48@gmail.com>
2013-04-28 21:06:04 -0500

    ShowInfoActivity initial commit

--- src/com/MSSE/MobileDVR/MainActivity.java
+++ src/com/MSSE/MobileDVR/MainActivity.java
@@ -1,9 +1,16 @@
 package com.MSSE.MobileDVR;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.HashMap;
 
 public class MainActivity extends Activity {
+    public static final String SHOW_INFO_ID = "showInfoID";
+    public static HashMap<Integer, ShowInfo> showRepo = new HashMap<Integer, ShowInfo>();
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +18,17 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        ShowInfo testShow = new ShowInfo("Mythbusters", "Greatest show on Earth");
+        showRepo.put(new Integer(1), testShow);
+
+        Button onlyButton = (Button)findViewById(R.id.show_button);
+        onlyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShowInfoActivity.class);
+                intent.putExtra(SHOW_INFO_ID, 1);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
---
+++ src/com/MSSE/MobileDVR/ShowInfoActivity.java
@@ -0,0 +1,35 @@
+package com.MSSE.MobileDVR;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Darin
+ * Date: 4/28/13
+ * Time: 4:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShowInfoActivity extends Activity {
+    private int showInfoID;
+    private ShowInfo showInfo;
+    private ShowTimeSlot showTimeSlot;
+    TextView showNameData;
+    TextView showChannelData;
+    TextView showTimeData;
+    TextView showDateData;
+    TextView showDescriptionData;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_details);
+        showInfoID = getIntent().getIntExtra(MainActivity.SHOW_INFO_ID, -1);
+        showInfo = MainActivity.showRepo.get(showInfoID);
+
+        showNameData = (TextView)findViewById(R.id.showInfoShowData);
+        showDescriptionData = (TextView)findViewById(R.id.showInfoDescription);
+        showNameData.setText(showInfo.getTitle());
+        showDescriptionData.setText(showInfo.getDescription());
+    }
+}
