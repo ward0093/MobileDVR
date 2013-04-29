31140690d3d2fa6e26597079c66ea320c8000c69
Kevin Hopps <kjhopps@gmail.com>
2013-04-28 20:18:42 -0500

    Did commit before saving these from Eclipse.

--- src/com/MSSE/MobileDVR/ChannelGuide.java
+++ src/com/MSSE/MobileDVR/ChannelGuide.java
@@ -1,8 +1,11 @@
 package com.MSSE.MobileDVR;
 
+import java.util.Date;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.LinearLayout;
 
 public class ChannelGuide extends Activity
 {
@@ -11,7 +14,7 @@ public class ChannelGuide extends Activity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_channel_guide);
+		setContentView(getContentView());
 	}
 
 	@Override
@@ -20,6 +23,27 @@ public class ChannelGuide extends Activity
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.channel_guide, menu);
 		return true;
+	}
+	
+	android.view.View getContentView()
+	{
+		LinearLayout result = (LinearLayout) findViewById(R.layout.activity_channel_guide);
+		
+		result.removeAllViews();
+		
+		ListingSource source = getListingSource();
+		
+		Channel[] channels = source.getChannels();
+		Date now = new Date();
+		int minutes = 24*60;
+		ShowTimeSlot[] timeSlots = source.getTimeSlots(now, minutes);
+		
+		return result;
+	}
+	
+	private ListingSource getListingSource()
+	{
+		return new DummyListingSource();
 	}
 
 }
--- src/com/MSSE/MobileDVR/ShowInfo.java
+++ src/com/MSSE/MobileDVR/ShowInfo.java
@@ -6,60 +6,6 @@ import android.graphics.drawable.Drawable;
 
 public class ShowInfo
 {
-	@Override
-	public int hashCode()
-	{
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
-		result = prime * result + ((image == null) ? 0 : image.hashCode());
-		result = prime * result + ((timeSlots == null) ? 0 : timeSlots.hashCode());
-		result = prime * result + ((title == null) ? 0 : title.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj)
-	{
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ShowInfo other = (ShowInfo) obj;
-		if (description == null)
-		{
-			if (other.description != null)
-				return false;
-		}
-		else if (!description.equals(other.description))
-			return false;
-		if (image == null)
-		{
-			if (other.image != null)
-				return false;
-		}
-		else if (!image.equals(other.image))
-			return false;
-		if (timeSlots == null)
-		{
-			if (other.timeSlots != null)
-				return false;
-		}
-		else if (!timeSlots.equals(other.timeSlots))
-			return false;
-		if (title == null)
-		{
-			if (other.title != null)
-				return false;
-		}
-		else if (!title.equals(other.title))
-			return false;
-		return true;
-	}
-
-	private ArrayList<ShowTimeSlot> timeSlots = new ArrayList<ShowTimeSlot>();
 	private String title = "";
 	private String description = "";
 	private Drawable image = null;
@@ -101,13 +47,6 @@ public class ShowInfo
 		return image;
 	}
 	
-	public ShowTimeSlot[] getTimeSlots()
-	{
-		ShowTimeSlot[] result = new ShowTimeSlot[timeSlots.size()];
-		timeSlots.toArray(result);
-		return result;
-	}
-	
 	private void setTitle(String title)
 	{
 		this.title = title != null ? title : "";
@@ -123,4 +62,49 @@ public class ShowInfo
 		this.image = image; // no check for null
 	}
 	
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((image == null) ? 0 : image.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ShowInfo other = (ShowInfo) obj;
+		if (description == null)
+		{
+			if (other.description != null)
+				return false;
+		}
+		else if (!description.equals(other.description))
+			return false;
+		if (image == null)
+		{
+			if (other.image != null)
+				return false;
+		}
+		else if (!image.equals(other.image))
+			return false;
+		if (title == null)
+		{
+			if (other.title != null)
+				return false;
+		}
+		else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
 }
