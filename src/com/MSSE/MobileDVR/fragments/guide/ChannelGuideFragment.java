fb2eaddd27dab50969af8e12f25bdd9887159bed
Alok <alok.gupta@pearson.com>
2013-05-06 22:51:15 -0500

    Added the search widget and updated the channel guide fragment to handle search events.

--- src/com/MSSE/MobileDVR/fragments/guide/ChannelGuideFragment.java
+++ src/com/MSSE/MobileDVR/fragments/guide/ChannelGuideFragment.java
@@ -8,6 +8,9 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+import android.widget.Toast;
 
 public class ChannelGuideFragment extends Fragment{
 	
@@ -30,6 +33,42 @@ public class ChannelGuideFragment extends Fragment{
 	  super.onPrepareOptionsMenu(menu);
 	  menu.clear();
 	  getActivity().getMenuInflater().inflate(R.menu.activity_tab_main, menu);
+	  //SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+	  SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+	  searchView.setQueryHint(getString(R.string.search_hint));
+	  // Tells your app's SearchView to use this activity's searchable configuration
+	  // searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+	  // searchView.setIconifiedByDefault(false);
+	    
+	  
+	    
+	    searchView.setOnSearchClickListener(new View.OnClickListener(){
+	    	@Override
+            public void onClick(View v) {
+	    		Toast toast = Toast.makeText(getActivity(), ((SearchView) v).getQuery().toString(), Toast.LENGTH_SHORT);
+	    		toast.show();
+	    	}
+	    });
+	    
+	    searchView.setOnQueryTextListener(new OnQueryTextListener() {
+			
+			@Override
+			public boolean onQueryTextSubmit(String query) {
+				Toast toast = Toast.makeText(getActivity(), query, Toast.LENGTH_SHORT);
+	    		toast.show();
+				
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public boolean onQueryTextChange(String newText) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		});
 	 }
+	
+	
 
 }
