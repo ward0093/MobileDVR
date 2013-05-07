85caa3bbfd25c902fb984f48327076b828b8efda
ward0093 <david.ward@capmation.com>
2013-05-06 22:19:42 -0500

    daves changes

--- src/com/MSSE/MobileDVR/fragments/recorded/MyShowsFragment.java
+++ src/com/MSSE/MobileDVR/fragments/recorded/MyShowsFragment.java
@@ -1,7 +1,25 @@
 package com.MSSE.MobileDVR.fragments.recorded;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.MSSE.MobileDVR.R;
 
 public class MyShowsFragment extends Fragment{
 
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.my_shows, container, false);
+
+        setMenuVisibility(true);
+        setHasOptionsMenu(true);
+
+        return view;
+    }
 }
